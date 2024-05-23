5de5d6d0d94ee7c770ceb2970a83189fecad95da
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-05-23 23:04:48 +0530

    marquee fixed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
 import "../components/component.css"
+import React, { useState, useEffect } from 'react';
 
 export const Nav = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
-    <div className='nav'>
+    <div className={scrolled ? 'nav-scrolled' : 'nav'}>
             <p>Services</p>
             <h3>Cyber Saviours</h3>
             <p>Contact</p>

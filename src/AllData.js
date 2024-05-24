151eae53bc223ba81aee582cbac298c230d2f013
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-05-24 10:35:24 +0530

    OurServices completed

---
+++ src/AllData.js
@@ -0,0 +1,41 @@
+export const services = [
+        {
+           id:1,
+           head: `Penetration Testing Services`,
+           description: `Proactive testing to uncover vulnerabilities in 
+           systems, ensuring robust defense against potential cyber threats.`,
+        },
+        {
+           id:2,
+           head: `SIEM Configuration Services`,
+           description: ` Centralized system for real-time security event analysis, 
+           enabling swift threat detection and response to security incidents.`,
+        },
+        {
+           id:3,
+           head: `SOC Analyst Services`,
+           description: `Continuous monitoring and analysis of security events to detect and mitigate potential threats, 
+           ensuring organizational resilience against cyber attacks.`,
+        },
+        {
+           id:4,
+           head: `System Auditing Services`,
+           description: `Comprehensive assessment of IT infrastructure to 
+           identify security vulnerabilities 
+           and strengthen overall security posture.`,
+        },
+        {
+           id:5,
+           head: `Firewall Assessment Services`,
+           description: `Evaluation of firewall configurations to ensure effective protection against cyber
+            threats and unauthorized access to network resources..`,
+        },
+        {
+           id:6,
+           head: `Penetration Testing Services`,
+           description: `Proactive testing to uncover vulnerabilities in 
+           systems, ensuring robust defense against potential cyber threats.`,
+        },
+       
+
+]
--- src/categories/OurServices.jsx
+++ src/categories/OurServices.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import "./ourServices.css"
+import { services } from '../AllData'
+import { ArrowRight } from '@phosphor-icons/react'
 
 export const OurServices = () => {
   return (
     <div className='container'>
-        OurServices
+      <div className='service-container'>
+        <h2>
+          Our Services.&nbsp;<span>Excellence in Every Step</span>
+        </h2>
+        <div className='card-container'>
+          {services.map((card) => (
+            <div className='card' key={card.id}>
+              <h3>{card.head}</h3>
+              <div className='card-description'> 
+              <p>{card.description}</p>
+              <div className='learn-more'>
+                Learn More
+              </div>
+              </div>
+
+            </div>
+          ))}
+        </div>
+        <div className='blob'></div>
+        <div className='blob1'></div>
+      </div>
+
     </div>
   )
 }

dee75ce5a4235f938aa21fce950f8f2b5f1a8eec
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-06-04 23:31:11 +0530

    responsive nav

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
     <Router>
-      {/* <Nav/> */}
+      <Nav/>
       <Routes>
         <Route path="/" element={<HomePage />}/>      
         <Route path="/services" element={<OurServices />}/>      

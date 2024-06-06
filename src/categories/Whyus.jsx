cd683917c188725e337eb17a5d65f63be47ac0c4
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-06-06 17:51:24 +0530

    responsive added to why us section

--- src/categories/Whyus.jsx
+++ src/categories/Whyus.jsx
@@ -1,32 +1,90 @@
 import React from 'react'
 import "./whyus.css"
-import { whyUs } from '../AllData'
-import { SimpleSpline } from '../categories/SimpleSpline'
+import { Minus, Sword, ShieldCheck, Gear } from '@phosphor-icons/react'
 
 
 export const Whyus = () => {
+    const info = [
+        {
+            head: "Threat-Proof Solutions",
+            descr: `Our defense mechanisms are in a constant state of evolution, 
+            ensuring an impenetrable shield against emerging cyber threats.`,
+        },
+        {
+            head: "Cutting-edge Defense",
+            descr: `We continuously pioneer new approaches to strengthen your digital defenses, 
+            with the latest innovations in cybersecurity.`,
+        },
+        {
+            head: "Dynamic Security Solutions",
+            descr: `With a focus on continual improvement, we offer dynamic security 
+            solutions that adapt and evolve alongside evolving cyber risks.`,
+        },
+
+    ]
     return (
         <div className='why-us-container'>
-                <div className='section-logo'></div>
-                <div className='whyUs-description'>
-                    <h3>Why choose us? <span>Unmatched Quality and Commitment</span></h3>
-                    <p className='lg-screen'>
-                        We offer comprehensive, tailored cybersecurity<br />
-                        solutions with a client-centric approach, providing <br />
-                        proactive defense, rapid response,and continuous innovation to <br />
-                        ensure your business stays secure and ahead of emerging threats.
-                    </p>
-                    <p className='ss-screen'>
-                        We offer comprehensive, tailored cybersecurity
-                        solutions with a <br />client-centric approach, providing <br />
-                        proactive defense, rapid response,<br />and continuous innovation to 
-                        ensure<br /> your business stays secure and<br /> ahead of emerging threats.
-                    </p>
+            <div className='whyUs-head'>
+                <Minus size={46} color="#FFFFFF" weight="thin" />
+                About us
+            </div>
+            <div className='section-info'>
+                <div className='section-description'>
+                    <h3>Empowering You with Reliable,
+                        Expert Services You Can Trust
+                    </h3>
+                <div className='section-logo ss-screen'></div>
 
+                    <div className='description-container'>
+                        <div className='descr-icons'>
+                            <div className='svg'>
+                                <Sword size={32} color="#e8e8e8"  />
+                                <div className='vertical'></div>
+                            </div>
+                            <div className='svg'>
+                                <ShieldCheck size={32} color="#e8e8e8"  />
+                                <div className='vertical'></div>
+
+                            </div>
+                            <div className=''>
+                            <Gear size={32} color="#e8e8e8" />
+
+
+                            </div>
+
+                        </div>
+                        <div className='descr-info'>
+                            {info.map((i, index) => (
+                                <div key={index}>
+                                    <h6>{i.head}</h6>
+                                    <p>{i.descr}</p>
+                                </div>
+                            ))}
+                        </div>
+
+                    </div>
 
                 </div>
-                <div className='gradient'></div>
-                    {/* <div className='why-blob1'></div> */}
+                <div className='section-logo lg-screen'></div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <div className='gradient g-1'></div>
+            <div className='gradient g-2'>
+            </div>
+            {/* <div className='why-blob1'></div> */}
 
         </div>
     )
@@ -48,14 +106,3 @@ export const Whyus = () => {
 
 
 
-{/* <div className='why-us-card-container'>
-                    {whyUs.map((card) => (
-                        <div className='why-card' key={card.id}>
-                            
-                            <h3>{card.head}</h3>
-                            <p>{card.description}</p>
-                            
-                        </div>
-                    ))}
-                   
-                </div> */}
